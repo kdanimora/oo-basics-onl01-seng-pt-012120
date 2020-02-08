10f130b764958d72e283f223cd7ee3fa7ddeada6
Karen Daniela <kdaniimora@gmail.com>
2020-02-08 23:38:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+
+class Shoe 
+  
+def initialize(brand)
+  @brand = brand 
+end 
+
+attr_accessor :brand, :color, :size, :material, :condition 
+attr_reader :brand 
+
+def
+end 
+
+end 
+
